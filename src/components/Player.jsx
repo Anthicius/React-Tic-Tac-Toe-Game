ce0d73590057a22fc148c7201a4886e347de39ce
Anthicius <andrius.makseckas@gmail.com>
2025-03-16 17:36:59 +0200

    Added: 1.  Dynamic state values isEditing, playerName for data adjustment and player names. 2. HandleClick function for updating changing between Edit and Save button state. Plus by passing function to get the latest previous state.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import { useState } from "react";
 
-const Player = ({name, symbol}) => {
-    return (
-        <li>
-        <span className="player">
-          <span className="player-name">{name}</span>
-          <span className="player-symbol">{symbol}</span>
-        </span>
-        <button>Edit</button>
-      </li>
-    )
-}
+const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
 
-export default Player
+  const handleClick = () => {
+    setIsEditing(prevEditing=> !prevEditing);
+  };
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={(e) => setPlayerName(e.target.value)}
+          />
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClick}>{!isEditing ? "Edit" : "Save"}</button>
+    </li>
+  );
+};
+
+export default Player;
